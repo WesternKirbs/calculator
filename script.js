88f21539af1ab5ff38f330094fef79dceb4e24b1
WesternKirbs <benlin96186@gmail.com>
2025-03-25 14:51:22 +0800

    finished eqaution logistics

--- script.js
+++ script.js
@@ -7,17 +7,15 @@ const multiply = (n1,n2) => n1*n2;
 const operate =  function (n1, o, n2) {
     switch(o){
         case '+':
-            add(n1,n2);
-            break;
+            return add(n1,n2);
         case '-':
-            subtract(n1,n2);
-            break;
+            return subtract(n1,n2);
         case '*':
-            multiply(n1,n2);
-            break;
+            return multiply(n1,n2);
         case '/':
-            divide(n1,n2);
-            break;
+            return divide(n1,n2);
+        case '=':
+            return n1;
         default:
             console.log("error");
     }
@@ -27,6 +25,7 @@ const container = document.querySelector(".container");
 const display = document.querySelector(".display");
 let n1 = 0;
 let n2 = o = undefined;
+let oe = 0;
 for(let i = 0; i<5; i++) {
     const row = document.createElement("div")
     row.setAttribute("class", "row")
@@ -35,20 +34,49 @@ for(let i = 0; i<5; i++) {
         if(Number.isInteger(+buttons[j])){
             btn.setAttribute("id", "num");
             btn.addEventListener("click", (e) =>{
-                if(o === undefined){
-                    if(n1 == 0) {
+                if(o === undefined || oe){
+                    if(n1 == 0 || oe == 1) {
                         n1 = buttons[j];
+                        if(oe)
+                            oe += 1;
                     }
                     else {
                         n1 += buttons[j];
                     }
+                    display.textContent = n1;
                 }
-                display.textContent = n1;
+                else{
+                    if(n2 === undefined || n2 == 0){
+                        n2 = buttons[j];
+                    }
+                    else {
+                        n2 += buttons[j];
+                    }
+                    display.textContent = n2;
+                }
             })
-
         }
         else{
             btn.setAttribute("id", buttons[j]);
+            btn.addEventListener("click", (e) =>{
+                if(n2 !== undefined) {
+                    if(buttons[j] === '='){
+                        oe = 1;
+                        n1 = operate(+n1, o, +n2);
+                        display.textContent = n1;
+                    }
+                    else{
+                        if(!oe)
+                            n1 = operate(+n1, o, +n2);
+                        n2 = undefined;
+                        oe = 0;
+                        display.textContent = n1;
+                    } 
+                }
+                if(buttons[j] !== '=')
+                    o = buttons[j];
+            })
+            
         }
             
         btn.textContent = buttons[j];
